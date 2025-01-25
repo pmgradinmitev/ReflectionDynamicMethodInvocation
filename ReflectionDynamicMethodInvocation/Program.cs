c909f7d821440820166681e3d097899fcbd92874
pmgradinmitev <pmg.tadinmitev92@gmail.com>
2025-01-25 22:12:43 +0200

    Add project files.

---
+++ ReflectionDynamicMethodInvocation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace ReflectionDynamicMethodInvocation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            // Get type information
+            Type type = typeof(Calculator);
+
+            // Get the method info for "Add"
+            MethodInfo method = type.GetMethod("Add");
+
+            // Invoke the method dynamically
+            object result = method.Invoke(calculator, new object[] { 5, 3 });
+
+            Console.WriteLine($"Result: {result}"); // Output: Result: 8
+        }
+    }
+}
